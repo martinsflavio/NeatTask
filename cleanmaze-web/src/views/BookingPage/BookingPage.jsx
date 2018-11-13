5fb7b87a2878b7beeffbb5f6e29cb30f37183082
martinsflavio <flaviodosreismartins@gmail.com>
2018-11-12 20:49:49 -0800

    react-routeer-setup NOT FINISHED

---
+++ cleanmaze-web/src/views/BookingPage/BookingPage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class BookingPage extends Component {
+  render() {
+
+    return (
+      <div>BookingPage</div>
+    )
+  }
+}
+
+export default BookingPage;
