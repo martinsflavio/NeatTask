5d8e5ee16f977dbc0c0ee83bc5bd44c5d889dea2
martinsflavio <flaviodosreismartins@gmail.com>
2018-12-17 23:50:33 -0800

    deleting list of beds and batths

--- neatTaskWeb/src/containers/BookingForm/BookingForm.jsx
+++ neatTaskWeb/src/containers/BookingForm/BookingForm.jsx
@@ -12,9 +12,6 @@ import { ButtonFullWidth } from "../../components";
 // Jss
 import styles from "./style.js";
 
-//TODO: abstracting list of beds to backend
-const optionsList = [0,1,2,3,4,5,6,7,8,9,10];
-
 class BookingForm extends Component {
   state = {
     isFormValid: false,
