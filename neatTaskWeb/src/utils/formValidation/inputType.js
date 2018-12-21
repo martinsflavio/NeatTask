2407b80b040b4f23640dfef65cd76e0de95d1b9a
martinsflavio <flaviodosreismartins@gmail.com>
2018-12-21 00:05:36 -0800

    refactoring and adding validations to more inputs

--- neatTaskWeb/src/utils/formValidation/inputType.js
+++ neatTaskWeb/src/utils/formValidation/inputType.js
@@ -1,2 +1,6 @@
 export const zipCode = "zipCode";
 export const email   = "email";
+export const beds    = "beds";
+export const baths   = "baths";
+export const date    = "date";
+export const time    = "time";
