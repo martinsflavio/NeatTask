ee16e911d869939625281d3eb11d48da8608c536
martinsflavio <flaviodosreismartins@gmail.com>
2018-12-28 00:54:24 -0800

    complete form strategy, validating inputs onBlur and onSubmit, all the validation is abstracted into 2 func iValidator and fValidator.

--- neatTaskWeb/src/utils/formValidation/index.js
+++ neatTaskWeb/src/utils/formValidation/index.js
@@ -1,11 +1,11 @@
-import inputValidator from "./validators/inputValidator";
-import formValidator from "./validators/formValidator.js";
-import * as inputTypes from "./constantTypes/inputTypes.js";
+import iValidator from "./validators/iValidator";
+import fValidator from "./validators/fValidator.js";
+import * as iTypes from "./constantTypes/inputTypes.js";
 import * as rulesTypes from "./constantTypes/rulesTypes.js"
 
 export {
-  inputValidator,
-  formValidator,
-  inputTypes,
+  iValidator,
+  fValidator,
+  iTypes,
   rulesTypes
 }
--- neatTaskWeb/src/utils/formValidation/validators/fValidator.js
+++ neatTaskWeb/src/utils/formValidation/validators/fValidator.js
@@ -1,18 +1,28 @@
-import inputValidator from "./inputValidator.js";
+import iValidator from "./iValidator.js";
 import objDeepCopy from "../../objDeepCopy";
 import makeObjIterable from "../../makeObjIterable";
 
-const formValidator = form => {
-  const iterableForm =  makeObjIterable(objDeepCopy(form));
-  let formBoolList = [];
+const fValidator = form => {
+  const iterableForm  = makeObjIterable(objDeepCopy(form));
+  const validatedForm = objDeepCopy(form);
+  const isFormValid   = [];
 
+  //loop truth the formObj
   for (let input of iterableForm) {
-    let { key } = input, { value } = input;
-      console.log(key, value);
-      formBoolList.push(inputValidator(key, value))
+    let { key, value }    = input,
+        validatedInputObj = iValidator(key, value);
+
+    // replace current inputObj for a validated version
+    validatedForm[key] = {...validatedInputObj};
+    // concat all isValid properties into a array
+    isFormValid.push(validatedInputObj.isValid);
   }
-  console.log(formBoolList);
-  return formBoolList.every(item => item === true);
+
+  // update formValidation property and return it
+  return {
+    isFormValid: isFormValid.every(result => result === true),
+    validatedForm: validatedForm
+  };
 };
 
-export default formValidator;
+export default fValidator;
