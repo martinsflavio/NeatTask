8dd33e4dbae474526d9a0c2206c5546952b14ae0
martinsflavio <flaviodosreismartins@gmail.com>
2019-01-05 21:10:47 -0800

    builing Booking View

--- neatTaskWeb/src/app/views/BookingPage/BookingPage.jsx
+++ neatTaskWeb/src/app/views/BookingPage/BookingPage.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
+import { BookingForm } from "../../containers";
+import { HeaderCustom } from "../../components";
+
 class BookingPage extends Component {
   render() {
     return (
       <div>
-        BookingPage
+        <HeaderCustom/>
+        <BookingForm/>
       </div>
     )
   }
