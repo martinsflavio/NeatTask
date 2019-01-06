73d5fadc0dc4fcd8bf6b8db6e8ccba6e2e8dfc55
martinsflavio <flaviodosreismartins@gmail.com>
2019-01-05 23:30:38 -0800

    changing header

--- neatTaskWeb/src/app/components/index.js
+++ neatTaskWeb/src/app/components/index.js
@@ -1,6 +1,6 @@
 import GridContainer from "./Grid/GridContainer.jsx";
 import GridItem from "./Grid/GridItem.jsx";
-import HeaderCustom from "./Header/HeaderCustom.jsx";
+import HeaderCustom from "./HeaderCustom/HeaderCustom.jsx";
 import ButtonFullWidth from "./Button/ButtonFullWidth.jsx";
 
 export {
